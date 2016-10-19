a6e5f7af6534dfc0e2fd74a0d72a525adade508c
unknown <>
2016-10-19 19:59:38 +0800

    添加新闻列表功能

--- News/Controllers/HomeController.cs
+++ News/Controllers/HomeController.cs
@@ -20,6 +20,41 @@ namespace News.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 欢迎页面 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Welcome()
+        {
+            var a = 100;
+            var sum = 0;
+            for (var i = 1; i <= a; i++)
+            {
+                sum += i;
+            }
+
+            ViewBag.sum = sum;
+            return View();
+        }
+
+
+        /// <summary>
+        /// 新闻列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+            string[] data= new string[] { "台风逼近广西景区关闭学校停课",
+                "广西每年入境游客超300万" ,
+                "空军招飞初选10月24日开始",
+                "柳州城市轨道交通勘探工作已展开"};
+
+            //ViewBag.data = data;
+            //ViewData["data"] = data;
+            ViewData.Model = data;
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
